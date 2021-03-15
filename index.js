4b7a729a8155415572ca52b8a44c2f8da6719aea
Konstantin Pfafenrod <63261467+fox-coding@users.noreply.github.com>
2021-03-15 23:20:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+class logger {
+    constructor(value, message) {
+        this.value = value;
+        this.message = message;
+    }
+}
 
+exports.createLogEntry = (value, message) => {
+    const log = new logger(value,message);
+    return `Saved ${value} Log, with the Message: ${message}`
+}
