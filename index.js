4e43b134f9c4c953af3357dc5d66162364659090
Konstantin Pfafenrod <63261467+fox-coding@users.noreply.github.com>
2021-03-22 07:13:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-class logger {
-    constructor(value, message) {
-        this.value = value;
-        this.message = message;
+const { createLogger, format, transports } = require('winston');
+const moment = require('moment-timezone')
+const { prettyPrint } = format;
+
+
+/**
+ * Logt Informationen in ein LogFile
+ */
+class Logger {
+
+    /**
+     *   Erstellt ein Logger Objekt der Klasse mit dem benötigten Path fürs Logfile
+    */
+    constructor() {
+        let today = moment().tz('Europe/Berlin').format('YYYY-MM-DDTHH-mm')
+
+        this.logger = createLogger({
+            format:
+                prettyPrint(),
+            transports: [
+                new transports.File({
+                    filename: `./logfiles/${today}-sl-trackingnumber-uploader.log`,
+                }),
+                new transports.Console({
+                    format: format.combine(
+                        format.colorize(),
+                        format.simple()
+                    )
+                })]
+        })
+    }
+
+    /**
+     * Schreibt die Informationen in das LogFile weg
+     * @param {object} options Logging Optionen (level default 'info')
+     * @param {String} [options.level='info'] error, warn, info, debug sind möglich
+     * @param {String} options.message Lognachricht
+     */
+    async log({ level = 'info', message } = {}) {
+        let now = moment().tz('Europe/Berlin').format('YYYY-MM-DD HH:mm:ss')
+
+        this.logger.log({
+            date: now,
+            level: level,
+            message: message
+        });
     }
 }
-
-exports.createLogEntry = (value, message) => {
-    const log = new logger(value,message);
-    return `Saved ${value} Log, with the Message: ${message}`
-}
+let logger = new Logger()
+module.exports = logger
